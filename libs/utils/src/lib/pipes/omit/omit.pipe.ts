ed243812308ee13fe5c607a8a89b62432979bc55
chandrasekhar09 <chandrasekhar.kommi@outlook.com>
2023-02-24 14:07:47 +0530

    chore(libs/utils): create omit pipe

--- libs/utils/src/lib/pipes/omit/omit.pipe.ts
+++ libs/utils/src/lib/pipes/omit/omit.pipe.ts
@@ -1,12 +1,30 @@
 import { Pipe, PipeTransform } from '@angular/core'
-
+import { isArray, isObject, isString } from 'lodash'
 @Pipe({
   name: 'omit',
 })
 export class OmitPipe implements PipeTransform {
-  transform(inputObject: any, itemsToRemove: string[] | string): unknown {
-    console.log(itemsToRemove)
+  transform(obj: any, propList: string[] | string) {
+    const newObj = { ...obj }
+    const result: any = {}
 
-    if (isNaN(itemsToRemove)) return inputObject
+    if (isString(propList) && isObject(obj)) {
+      for (const key in obj) {
+        if (key !== propList) {
+          result[key] = newObj[key]
+        }
+      }
+      return result
+    }
+
+    if (isArray(propList) && isObject(obj)) {
+      Object.keys(obj).forEach((key) => {
+        if (!propList.includes(key)) {
+          result[key] = newObj[key]
+        }
+      })
+      return result
+    }
+    return obj
   }
 }
