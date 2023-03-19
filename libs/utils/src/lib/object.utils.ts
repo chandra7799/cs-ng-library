d7c166d1dcc91d5c66760bb2294b740d3fb24ff2
chandrasekhar09 <chandrasekhar.kommi@outlook.com>
2023-03-19 14:08:20 +0530

    chore(lib/utils): utils change

--- libs/utils/src/lib/object.utils.ts
+++ libs/utils/src/lib/object.utils.ts
@@ -1,7 +1,6 @@
 import { isObject, keysIn as _keysIn } from 'lodash'
 
-// eslint-disable-next-line @typescript-eslint/ban-types
 export function keysIn(object: any): string[] {
   if (!isObject(object)) return []
-  return _keysIn(object)
+  return _keysIn(object) as string[]
 }
